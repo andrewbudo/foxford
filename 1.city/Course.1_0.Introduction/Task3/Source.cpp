1fb770a2ba6d5e34a19f387780c0bb2e8429ce64
Budo_A <Budo_A@credo-dialogue.com>
2019-08-13 00:03:29 +0300

    task3 course1

--- 1.city/Course.1_0.Introduction/Task3/Source.cpp
+++ 1.city/Course.1_0.Introduction/Task3/Source.cpp
@@ -3,43 +3,66 @@
 #include <algorithm>
 using namespace std;
 
+struct Bet {
+	int A = -1;
+	int B = -1;
+	int C = -1;
+	int D = -1;
+};
+
 int main() {
 	int K; // number of cockroaches [2; 10]
-	int N; // number of funs [1;100];
+	int N; // number of funs [1; 100];
 	cin >> K >> N;
-	for (int i = 0; i < N; ++i) 
-	{
-		int A, B, C, D;
-		cin >> A >> B >> C >> D;
-	}
-	vector<int> allCockroaches;
-	for (int i = 1; i <= K; ++i) 
-	{
-		allCockroaches.push_back(i);
-	}
-	vector< vector<int> > combinations;
-	// combinations.push_back(allCockroaches);
 
+	vector<Bet> bets;
+	for (int i = 0; i < N; ++i) {
+		Bet b;
+		cin >> b.A >> b.B >> b.C >> b.D;
+		bets.push_back(b);
+	}
+
+	vector<int> all(K);
+	for (int i = 0; i < K; ++i) {
+		all.at(i) = i + 1;
+	}
+
+	bool flag;
 	do {
-		combinations.push_back(allCockroaches);
-	} while (next_permutation(allCockroaches.begin(),allCockroaches.end()));
+		flag = true;
+		for (auto b : bets) {
+			std::vector<int>::iterator it1 = std::find(all.begin(), all.end(), b.A);
+			std::vector<int>::iterator it2 = std::find(all.begin(), all.end(), b.B);
+			std::vector<int>::iterator it3 = std::find(all.begin(), all.end(), b.C);
+			std::vector<int>::iterator it4 = std::find(all.begin(), all.end(), b.D);
 
+			int d1 = std::distance(all.begin(), it1);
+			int d2 = std::distance(all.begin(), it2);
+			int d3 = std::distance(all.begin(), it3);
+			int d4 = std::distance(all.begin(), it4);
 
-	/*
-	// bubble sort
-	for (size_t j = 0; j < allCockroaches.size(); j++) 
-	{
-		for (size_t i = 0; i < allCockroaches.size() - 1; i++)
-		{
-			if (allCockroaches.at(i) < allCockroaches.at(i + 1)) {
-				int temp = allCockroaches.at(i);
-				allCockroaches.at(i) = allCockroaches.at(i + 1);
-				allCockroaches.at(i + 1) = temp;
-				combinations.push_back(allCockroaches);
+			if (it1 != all.end() && it2 != all.end() && it3 != all.end() && it4 != all.end()) {
+				if ((d1 < d2 && d3 >= d4) || (d1 >= d2 && d3 < d4)) {
+				}
+				else {
+					flag = false;
+					break;
+				}
 			}
+			else {
+				flag = false;
+				break;
+			}
+			
 		}
-	}
-	*/
+		if (flag) {
+			for (auto a : all) {
+				cout << a << " ";
+			}
+			return 0;
+		}
+	} while (next_permutation(all.begin(),all.end()));
 
+	cout << 0 << "\n";
 	return 0;
 }
